95cf1dd7928a34b30a31bdad27a356d1f46c9d11
Riley <wardriley1@gmail.com>
2024-02-19 15:27:52 +0000

    changed home

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,14 @@
-export default function Home() {
+export default async function Home() {
+  const reviews = await sql`SELECT * FROM reviews`;
   return (
     <div>
     <h2>Home</h2>
     <h3>This is home</h3>
+    <ul>
+      {reviews.rows.map((review) => {
+        <li key = {review.id}>{review.album_title}</li>
+})}
+    </ul>
     </div>
   )
 }
