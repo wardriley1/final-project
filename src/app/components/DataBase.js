f5f07a7b5a78bfdc2a28953180da2c4fdd33bec9
myles-hash <mylesbillington@gmail.com>
2024-02-21 09:27:03 +0000

    include files

---
+++ src/app/components/DataBase.js
@@ -0,0 +1,29 @@
+import { sql } from "@vercel/postgres"
+import Link from "next/link";
+
+
+export default async function DataBase() {
+    const dbReviews = await sql `SELECT * FROM reviews`;
+
+    console.log("SAVED REVIEWS", dbReviews);
+
+  return (
+    <div>
+        <h1>SAVED ALBUMS:</h1>
+         <ul>
+                    {dbReviews.rows.map( (dbReview) => {
+                      return (
+                        <div className="albumContainer" key={dbReview.album_id}>
+                        <img src={`${dbReview.album_image_url}`} />
+                        <h3>{dbReview.album_name}</h3>
+                        <Link href={`${dbReview.spotify_link}`}>Play</Link>
+                        <p>Artist: {dbReview.album_artist}</p>
+                        <p>Score: {dbAlbum.album_score}</p>
+                        <p>Review: {dbAlbum.album_review}</p>
+                        </div>
+                      )
+                    })}
+                </ul>
+    </div>
+  )
+}
