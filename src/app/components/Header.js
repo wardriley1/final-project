454b09942608f2c81e5dc7f134aa0cbda0b76896
Artur <artur3087@gmail.com>
2024-02-22 14:06:42 +0000

    werecat

--- src/app/components/Header.js
+++ src/app/components/Header.js
@@ -1,8 +1,21 @@
+// Header component //
+
+
+// IMPORTing below:
+"use client"
+import * as React from "react";
+import { motion } from "framer-motion"; // import motion variable //
+
+
 export default function Header(){
 
   return (
     <header>
-      <h1 className="head">Album Reviews</h1>
+
+    <motion.div animate = {{x: 500, y: 0}} >
+      <h1 className="head">🎼 ♪ ♪ ♪ ♫ RiffRater ♫ ♪ ♪ ♪ 🎸 </h1>
+    </motion.div>
+
     </header>
   );
 
