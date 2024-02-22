989395a5d24bdadb967cc5582186ec8ec4cc0756
Artur <artur3087@gmail.com>
2024-02-22 09:46:07 +0000

    Artur 22 Feb 9:45

--- .next/server/webpack-runtime.js
+++ .next/server/webpack-runtime.js
@@ -47,11 +47,6 @@
 /******/ 	__webpack_require__.m = __webpack_modules__;
 /******/ 	
 /************************************************************************/
-/******/ 	/* webpack/runtime/amd options */
-/******/ 	(() => {
-/******/ 		__webpack_require__.amdO = {};
-/******/ 	})();
-/******/ 	
 /******/ 	/* webpack/runtime/compat get default export */
 /******/ 	(() => {
 /******/ 		// getDefaultExport function for compatibility with non-harmony modules
@@ -130,7 +125,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("633b9a15d8b346b4")
+/******/ 		__webpack_require__.h = () => ("f88a60df28f61033")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/hasOwnProperty shorthand */
--- src/app/page.js
+++ src/app/page.js
@@ -1,3 +1,5 @@
+// HOME PAGE //
+
 import { sql } from "@vercel/postgres";
 import BackPicture from '@/app/components/BackPicture';
 
@@ -11,7 +13,7 @@ export default async function Home() {
           <div className="homecontent">
             <h2>RiffRater by MARD Disc-cuss.</h2>
              <h3>Reviews you can trust!</h3>
-                <p>RiffRater! This is the only website which allows you to post about your favourite music albums</p>
+                <p>RiffRater! This is the website designed for you to post about your favourite music albums!</p>
                 <p>You can now post your reviews and recommend tracks You love!</p>
                 
           </div>
